ae091509e4a7ae5d0c684c225c3c05601b66a0fb
NATHAN YAU <nshyau@gmail.com>
2023-02-06 11:09:49 -0800

    Update calculate_pay.py with docstring and doctest

--- calculate_pay.py
+++ calculate_pay.py
@@ -1,4 +1,21 @@
 def calculate_pay(hours, wage):
+    """
+    Calculate the employee's weekly salary
+
+    :param hours: floating point number
+    :param wage: floating point number
+    :precondition: hours must be a float point number or an integer
+    :precondition: wage must be a float point number or an integer
+    :postcondition: calculate the weekly pay
+    :return: employee's weekly salary as a floating point number
+
+    >>> calculate_pay(10.5, 20)
+    210.0
+    >>> calculate_pay(15, 20.5)
+    307.5
+    >>> calculate_pay(58, 30.0)
+    2280.0
+    """
     if hours > 40:
         weekly_pay = wage * 40
         weekly_pay += 2 * (wage * (hours - 40))
